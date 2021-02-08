0162bb5460e096da8a11e3129eaa6ab35a916bdc
AndreyUA <infernalsteel666@gmail.com>
2021-02-08 19:01:55 +0200

    deploy

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
 
 app.use(express.json({ extended: false }));
 
+/*
 app.get("/", (req, res) => {
   res.send("api is running");
 });
+*/
 
 // Routes
 // register new user
@@ -19,6 +22,16 @@ app.use("/api/login", require("./routes/login"));
 app.use("/api/posts", require("./routes/posts"));
 // profiles
 app.use("/api/profile", require("./routes/profile"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
