8a6eb73b5c2b052b54d13baf4d48646a67302a35
OlhaZhyharieva <olhavelyka05@gmail.com>
2021-05-07 21:32:59 +0300

    add pagination from  material ui

--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
-// Components
-import PaginationButtons from "../PaginationButtons";
+import { rgba } from "polished";
+import Pagination from "@material-ui/lab/Pagination";
 
 // Styled components
 const PaginationWrapper = styled.div`
@@ -11,52 +10,56 @@ const PaginationWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   padding: 20px;
-  height: 100px;
-  width: 750px;
-  background-color: #edeeef;
+  background-color: transparent;
   margin: 0 auto;
   border-radius: 10px;
-`;
-const PaginationArrowButton = styled.button`
-  width: 40px;
-  height: 26px;
-  border-radius: 5px;
-  background-color: #1cbf1c;
-  outline: none;
-  border: 0;
-  cursor: pointer;
-  transition: opacity 0.3s ease-in-out;
 
-  &:hover {
-    opacity: 0.6;
+  .material-pagination {
+    background-color: #edeeef;
+    padding: 10px;
+    border-radius: 10px;
+
+    &::after {
+      display: none;
+    }
+
+    button.Mui-selected {
+      background-color: #0060fc;
+    }
+
+    .MuiPaginationItem-page:hover {
+      background-color: ${rgba("#0060fc", 0.4)};
+    }
   }
 `;
 
 // React component
-const Pagination = ({ quantity, navPostsHandler, actualNumber }) => {
+const PaginationBlock = ({ quantity, navPostsHandler, actualNumber }) => {
   return (
     <PaginationWrapper>
-      <PaginationArrowButton onClick={() => navPostsHandler(actualNumber - 1)}>
-        &#60;
-      </PaginationArrowButton>
-      <PaginationButtons
-        qty={quantity}
-        active={actualNumber}
-        handleChange={navPostsHandler}
+      <Pagination
+        classes={{
+          root: "material-pagination",
+          button: "material-label",
+        }}
+        size="large"
+        count={quantity}
+        color="primary"
+        page={actualNumber + 1}
+        onChange={(_, actualNumber) => {
+          navPostsHandler(actualNumber - 1);
+        }}
       />
-      <PaginationArrowButton onClick={() => navPostsHandler(actualNumber + 1)}>
-        &#62;
-      </PaginationArrowButton>
     </PaginationWrapper>
   );
 };
 
-Pagination.propTypes = {
+PaginationBlock.propTypes = {
   actualNumber: PropTypes.number.isRequired,
   quantity: PropTypes.number.isRequired,
   navPostsHandler: PropTypes.func.isRequired,
 };
 
-export default Pagination;
+export default PaginationBlock;
--- client/src/components/PaginationButtons/index.jsx
+++ client/src/components/PaginationButtons/index.jsx
@@ -1,66 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-
-// Styled components
-const PaginationButtonsWrapper = styled.div``;
-const PaginationNavButton = styled.button`
-  width: 40px;
-  height: 26px;
-  border-radius: 5px;
-  background-color: ${(props) => (props.isActive ? "coral" : "#0060fc")};
-  outline: none;
-  border: 0;
-  cursor: pointer;
-  transition: opacity 0.3s ease-in-out;
-  margin: 0 10px;
-
-  &:hover {
-    opacity: 0.6;
-  }
-`;
+import Pagination from "@material-ui/lab/Pagination";
 
 // React component
 const PaginationButtons = ({ qty, active, handleChange }) => {
-  const arrButton = [];
+  console.log(active);
 
-  if (qty < 10) {
-    for (let i = 1; i <= qty; i++) {
-      arrButton.push(
-        <PaginationNavButton
-          isActive={i - 1 === active}
-          onClick={() => handleChange(i - 1)}
-          key={i}
-        >
-          {i}
-        </PaginationNavButton>
-      );
-    }
+  return (
+    <Pagination
+      count={qty}
+      color="primary"
+      page={active + 1}
+      onChange={(_, active) => {
+        console.log(active);
 
-    return <PaginationButtonsWrapper>{arrButton}</PaginationButtonsWrapper>;
-  } else {
-    for (let i = 1; i <= 9; i++) {
-      arrButton.push(
-        <PaginationNavButton
-          isActive={i - 1 === active}
-          onClick={() => handleChange(i - 1)}
-          key={i}
-        >
-          {i}
-        </PaginationNavButton>
-      );
-    }
-
-    arrButton.push(<PaginationNavButton>&#8230;</PaginationNavButton>);
-
-    return <PaginationButtonsWrapper>{arrButton}</PaginationButtonsWrapper>;
-  }
-};
-
-PaginationButtons.propTypes = {
-  qty: PropTypes.number.isRequired,
-  active: PropTypes.number.isRequired,
-  handleChange: PropTypes.func.isRequired,
+        handleChange(active - 1);
+      }}
+    />
+  );
 };
 
 export default PaginationButtons;
